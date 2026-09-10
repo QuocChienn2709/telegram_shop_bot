1a22574698de9902f9e1fe122127942b0be841f9
𝐐𝐮𝐨̂́𝐜 𝐂𝐡𝐢𝐞̂́𝐧  <hackiosipa@gmail.com>
2026-09-11 02:08:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -12,9 +12,6 @@ class Config:
     PAYOS_CANCEL_URL = os.getenv("PAYOS_CANCEL_URL", "https://t.me/your_bot")
     PAYOS_RETURN_URL = os.getenv("PAYOS_RETURN_URL", "https://t.me/your_bot")
     WEBHOOK_URL = (os.getenv("WEBHOOK_URL") or "").rstrip("/")
-    MONGODB_URI = os.getenv(
-        "MONGODB_URI",
-        "mongodb+srv://quocchienn:chien207@cluster0.0swxhya.mongodb.net/?appName=Cluster0"
-    )
-    MONGODB_DB = os.getenv("MONGODB_DB", "telegram_shop")
+    MONGODB_URI = os.getenv("MONGODB_URI")
+MONGODB_DB = os.getenv("MONGODB_DB", "telegram_shop")
     ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip().isdigit()]
