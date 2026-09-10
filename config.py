4f836b334f780b67e1bd28eb312e266854ab53d6
𝐐𝐮𝐨̂́𝐜 𝐂𝐡𝐢𝐞̂́𝐧  <hackiosipa@gmail.com>
2026-09-11 06:15:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,24 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-
     PAYOS_CLIENT_ID = os.getenv("PAYOS_CLIENT_ID")
     PAYOS_API_KEY = os.getenv("PAYOS_API_KEY")
     PAYOS_CHECKSUM_KEY = os.getenv("PAYOS_CHECKSUM_KEY")
     PAYOS_CANCEL_URL = os.getenv("PAYOS_CANCEL_URL", "https://t.me/your_bot")
     PAYOS_RETURN_URL = os.getenv("PAYOS_RETURN_URL", "https://t.me/your_bot")
-
     WEBHOOK_URL = (os.getenv("WEBHOOK_URL") or "").rstrip("/")
-
-    # MongoDB
-    MONGODB_URI = os.getenv("MONGODB_URI")
+    MONGODB_URI = os.getenv(
+        "MONGODB_URI",
+        "mongodb+srv://quocchienn:chien207@cluster0.0swxhya.mongodb.net/?appName=Cluster0"
+    )
     MONGODB_DB = os.getenv("MONGODB_DB", "telegram_shop")
-
-    ADMIN_IDS = [
-        int(x)
-        for x in os.getenv("ADMIN_IDS", "").split(",")
-        if x.strip().isdigit()
-    ]
+    ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip().isdigit()]
